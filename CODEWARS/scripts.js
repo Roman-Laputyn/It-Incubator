2d1c0df59fd9127b3c83ff56747f8d099bb79a3c
Roman-Laputyn <laputyn.roman@gmail.com>
2023-04-09 15:08:16 -0600

    new task

--- CODEWARS/scripts.js
+++ CODEWARS/scripts.js
@@ -109,3 +109,8 @@ function countPositivesSumNegatives(input) {
     return Array(countPositive, sumNegative);
   }
 }
+
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
