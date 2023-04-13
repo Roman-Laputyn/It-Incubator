966b8d67039953f4681a6876cd5cabc242f08384
Roman-Laputyn <laputyn.roman@gmail.com>
2023-04-12 20:56:45 -0600

    new task

--- CODEWARS/scripts.js
+++ CODEWARS/scripts.js
@@ -132,3 +132,8 @@ const reverseSeq = (n) => {
 
 // function 'greet'
 const greet = () => 'hello world!';
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord(bool) {
+  return bool ? 'Yes' : 'No';
+}
